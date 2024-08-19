0bf5fa5710a8a2a25dcef5da0a444ceeafa8d7b0
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-08-19 12:57:07 +0500

    deploye

--- frontend/src/hooks/UseAuthChck.jsx
+++ frontend/src/hooks/UseAuthChck.jsx
@@ -9,7 +9,7 @@ const UseAuthChck = () => {
     try {
       console.log("Sending user details to backend:", user);
       const response = await axios.post(
-        "http://localhost:4000/api/user/register",
+        "https://olx-sap.vercel.app/api/user/register",
         {
           email: user.email,
           name: user.name,
