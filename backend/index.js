a4eb4c9a7b80325aadb2b2e29f4357b5c6216a03
Muhammad Sadam Muneer <sadammuneer39@gmail.com>
2024-08-19 12:07:13 +0500

    apply fiilter on home page also

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://olx-frontend-opal.vercel.app/", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 
